ad8ce865ed542ae3ae196be38c30f12b653e53fe
niteshyadav7 <nitesh0709yadav@gmail.com>
2023-08-28 02:05:44 +0530

    triangle

--- data_structure_/Basic/Pattern.java
+++ data_structure_/Basic/Pattern.java
@@ -32,12 +32,41 @@ public class Pattern {
     }
 
     public static void triangle(int n) {
+        int printed = 1;
+        int row = 1;
+        while (row <= n) {
+            int col = 1;
 
+            while (col <= row) {
+
+                System.out.print(printed);
+                printed += 1;
+                col++;
+
+            }
+            System.out.println();
+            row++;
+        }
+    }
+
+    public static void alphabaticalRect(int n) {
+        int row = 1;
+        while (row <= n) {
+            int col = 1;
+            while (col <= n) {
+                System.out.print((char) (64 + 1));
+                col++;
+            }
+            System.out.println();
+            row++;
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // rectangle2(n);
+        // triangle(n);
+        alphabaticalRect(n);
     }
 }
---
+++ data_structure_/imp.md
@@ -0,0 +1,3 @@
+type cating:
+
+(char)('A'+1)
